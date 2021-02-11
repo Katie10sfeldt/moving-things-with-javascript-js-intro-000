b92df1222e2d42c10b0b2339349516e6a3a001db
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-11 16:56:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var dodger = document.getElementById('dodger')
 document.addEventListener('keydown', (e) => {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace('px', '')
-    var left = paresInt(leftNumbers, 10);
+    var left = parseInt(leftNumbers, 10);
 
     dodger.style.left = `${left - 1}px`;
   }
